98e7fccd5c96a86caf0f29a9a8d7c027e78903ab
Esakkiammal <spesakki1@gmail.com>
2024-09-29 09:15:03 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import "./App.css";
+import FooterSection from "./Contents/Footer";
+import ProjectsSection from "./Contents/Projects";
+import HeaderSection from "./Contents/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex flex-col">
+      {/* Container for both boxes */}
+      <div className="flex flex-col md:flex-row flex-grow">
+        {/* First Box: Role Descriptions and Skills */}
+        <HeaderSection />
+        <ProjectsSection />
+      </div>
+
+      <FooterSection />
     </div>
   );
 }
